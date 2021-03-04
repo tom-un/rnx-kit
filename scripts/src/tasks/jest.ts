eca43b9cbe4c889d74785af209ebbfbaafd60791
Adam Foxman <afoxman@microsoft.com>
2021-03-04 08:54:20 -0800

    Initial draft of Jest infrastructure for platform-specific tests with snapshots. Includes a sample test with actual snapshots.
    This draft doesn't work. We use the same set of tests for each platform, but we have run Jest once per platform due to the way it is configured (haste object). Each run produces a platform-specific snapshot for the same test. The snapshot cache gets confused by this. Subsequent runs of any one platform lead to the snapshot detector saying that there are a number of obsolete snapshots. For example, running ios causes android, windows, and macos snapshots to show as obsolete.
    
    sdx-platform has solved this somehow. The code is very large and complex, and heavily tied to react-native.

--- scripts/src/tasks/jest.ts
+++ scripts/src/tasks/jest.ts
@@ -1,13 +1,31 @@
-import { argv, jestTask, JestTaskOptions } from "just-scripts";
+import { existsSync } from "fs";
+import { argv, jestTask, JestTaskOptions, logger } from "just-scripts";
 
-type JestPlatforms = "ios" | "android" | "windows" | "macos";
+export type JestPlatform = "ios" | "android" | "windows" | "macos";
 
 function createJestTask(
   options: JestTaskOptions,
-  platform: JestPlatforms | undefined
+  platform: JestPlatform | undefined
 ) {
   const config = platform ? `jest.config.${platform}.js` : "jest.config.js";
-  return jestTask({ ...options, config: config });
+  if (existsSync(config)) {
+    return jestTask({ ...options, config: config });
+  } else if (!platform) {
+    //  There's no Jest config file, but there might be Jest config embedded in
+    //  package.json. jestTask() will figure that out and only run when it is present.
+    //
+    //  If package.json config is used, it can only apply to the "default" platform.
+    //
+    //  Why? Jest doesn't know anything about platform-specific source files and tests.
+    //  We do a lot of tricks to make it work. Those tricks can't fit into a JSON file,
+    //  though -- they need to be in a .js config file. So if you want platform-specific
+    //  sources and tests, you need to use jest config files.
+    //
+    return jestTask({ ...options });
+  } else {
+    logger.warn(`no ${platform} jest configuration found, skipping jest`);
+    return undefined;
+  }
 }
 
 function getJestOptions(): JestTaskOptions {
